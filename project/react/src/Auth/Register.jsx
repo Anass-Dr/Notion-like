6f08941b15324016464ef8753c0257561103beb0
Anass-Dr <anasdrissi77@gmail.com>
2024-03-14 22:54:06 +0000

    Form submission added with page redirection

--- project/react/src/Auth/Register.jsx
+++ project/react/src/Auth/Register.jsx
@@ -1,7 +1,10 @@
-import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import Auth from "./Auth";
 import FormInput from "./FormInput";
 
 function Register() {
+    const nav = useNavigate();
     const [user, setUser] = useState({
         username: "",
         email: "",
@@ -17,61 +20,65 @@ function Register() {
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        const req = await fetch("http://127.0.0.1:8000/api/register", {
+        const res = await fetch("http://127.0.0.1:8000/api/register", {
             method: "post",
             headers: {
                 "content-type": "application/json",
             },
             body: JSON.stringify(user),
         });
-        const res = await req.json();
-        console.log(res);
+        const data = await res.json();
+
+        if (data.success) return nav("/login");
     };
 
     return (
-        <main>
-            <h2>Sign up</h2>
-            <form onSubmit={handleSubmit}>
-                <FormInput
-                    label="Username"
-                    name="username"
-                    type="text"
-                    placeholder="Enter your username..."
-                    value={user.username}
-                    onChange={handleInputChange}
-                />
-                <FormInput
-                    label="Email"
-                    name="email"
-                    type="email"
-                    placeholder="Enter your email address..."
-                    value={user.email}
-                    onChange={handleInputChange}
-                />
-                <FormInput
-                    label="Password"
-                    name="password"
-                    type="password"
-                    placeholder="Enter your password"
-                    value={user.password}
-                    onChange={handleInputChange}
-                />
-                <FormInput
-                    label="Confirm Password"
-                    name="password_confirmation"
-                    type="password"
-                    placeholder="Confirm your password"
-                    value={user.password_confirmation}
-                    onChange={handleInputChange}
-                />
-                <button type="submit">Submit</button>
-                <div className="form-footer">
-                    <p>
-                        Already have an account? <a href="/login">Login</a>
-                    </p>
-                </div>
-            </form>
-        </main>
+        <Auth>
+            <main>
+                <h2>Sign up</h2>
+                <form onSubmit={handleSubmit}>
+                    <FormInput
+                        label="Username"
+                        name="username"
+                        type="text"
+                        placeholder="Enter your username..."
+                        value={user.username}
+                        onChange={handleInputChange}
+                    />
+                    <FormInput
+                        label="Email"
+                        name="email"
+                        type="email"
+                        placeholder="Enter your email address..."
+                        value={user.email}
+                        onChange={handleInputChange}
+                    />
+                    <FormInput
+                        label="Password"
+                        name="password"
+                        type="password"
+                        placeholder="Enter your password"
+                        value={user.password}
+                        onChange={handleInputChange}
+                    />
+                    <FormInput
+                        label="Confirm Password"
+                        name="password_confirmation"
+                        type="password"
+                        placeholder="Confirm your password"
+                        value={user.password_confirmation}
+                        onChange={handleInputChange}
+                    />
+                    <button type="submit">Submit</button>
+                    <div className="form-footer">
+                        <p>
+                            Already have an account?{" "}
+                            <Link to="/login">Login</Link>
+                        </p>
+                    </div>
+                </form>
+            </main>
+        </Auth>
     );
 }
 
