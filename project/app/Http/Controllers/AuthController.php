6ab437f0b3a4ae02d84ad1fb6ad3d44686d157c7
Anass-Dr <anasdrissi77@gmail.com>
2024-03-14 16:24:53 +0000

    validation message added

--- project/app/Http/Controllers/AuthController.php
+++ project/app/Http/Controllers/AuthController.php
@@ -4,20 +4,31 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Services\JWT\GenerateToken;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        $validatedData = $request->validate([
-            'username' => ['required', 'string', 'min:3', 'unique:users'],
-            'email' => ['required', 'string', 'email', 'lowercase', 'unique:users'],
-            'password' => ['required', 'string', 'confirmed'],
-        ]);
+        try {
+            $rules = [
+                'username' => ['required', 'string', 'min:3', 'unique:users'],
+                'email' => ['required', 'string', 'email', 'lowercase', 'unique:users'],
+                'password' => ['required', 'string', 'confirmed'],
+            ];
 
-        $user = User::create($validatedData);
+            $this->validate($request, $rules);
+        } catch (ValidationException $e) {
+            return response()->json([
+                "message" => "Validation failed",
+                "errors" => $e->validator->errors()->messages(),
+            ], 422);
+        }
+
+        $user = User::create($request->only('username', 'email', 'password'));
         return response()->json($user, 201);
     }
 
