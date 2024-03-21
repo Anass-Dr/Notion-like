a6c903ec563fdc9ed6a023ac4c5553f34d9b3704
Anass-Dr <anasdrissi77@gmail.com>
2024-03-21 18:16:26 +0000

    id wildcard added for update pages route

--- project/routes/api.php
+++ project/routes/api.php
@@ -28,7 +28,7 @@ Route::middleware('jwt.auth')->group(function () {
   # Page Endpoint :
   Route::get('/pages', [PageController::class, 'index']);
   Route::post('/pages', [PageController::class, 'store']);
-  Route::put('/pages', [PageController::class, 'update']);
+  Route::put('/pages/{id}', [PageController::class, 'update']);
   Route::delete('/pages', [PageController::class, 'destroy']);
 });
 
