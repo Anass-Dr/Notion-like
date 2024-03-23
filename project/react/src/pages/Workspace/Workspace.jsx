4d5a410bc4cddfd42514af3448d127cf8bedc2f7
Anass-Dr <anasdrissi77@gmail.com>
2024-03-23 21:11:09 +0000

    Wrapping all workspace components with Wrapper component for Loading effect

--- project/react/src/pages/Workspace/Workspace.jsx
+++ project/react/src/pages/Workspace/Workspace.jsx
@@ -1,17 +1,22 @@
+import { useParams } from "react-router-dom";
 import Sidebar from "./Sidebar/Sidebar";
 import Header from "./Header/Header";
 import Page from "./Page/Page";
 import { WorkspaceContextProvider } from "../../context/WorkspaceContext";
+import Wrapper from "../../Wrapper";
 import "./main.css";
 
 function Workspace() {
+    const { username } = useParams();
     return (
         <div id="user">
             <div className="container">
                 <WorkspaceContextProvider>
-                    <Sidebar />
-                    <Header />
-                    <Page />
+                    <Wrapper>
+                        <Sidebar />
+                        <Header />
+                        <Page />
+                    </Wrapper>
                 </WorkspaceContextProvider>
             </div>
         </div>
