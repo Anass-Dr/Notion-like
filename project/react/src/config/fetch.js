493f7a2887e200bba164ebfcac926c3972774afc
Anass-Dr <anasdrissi77@gmail.com>
2024-03-23 23:06:36 +0000

    Add endpoint variable for fetch request

--- project/react/src/config/fetch.js
+++ project/react/src/config/fetch.js
@@ -1,5 +1,6 @@
 const jwt_token = localStorage.getItem("jwt_token");
 
+export const endpoint = "http://127.0.0.1:8000/api";
 export const headers = {
     "content-type": "application/json",
     Authorization: `Bearer ${jwt_token}`,
