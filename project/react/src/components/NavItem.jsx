e8cfa30ffa5cd72dd4a7709ceaf7387fccf6de34
Anass-Dr <anasdrissi77@gmail.com>
2024-03-24 22:32:53 +0000

    Logout functionality added to logout button

--- project/react/src/components/NavItem.jsx
+++ project/react/src/components/NavItem.jsx
@@ -1,12 +1,15 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { WorkspaceContext } from "../context/WorkspaceContext";
 import "./nav-item.css";
 
 function NavItem({ title, children }) {
+    const nav = useNavigate();
     const { handleNewPage } = useContext(WorkspaceContext);
 
     const handleClick = () => {
         if (title === "New page") handleNewPage();
+        if (title === "Logout") nav("/logout");
     };
 
     return (
