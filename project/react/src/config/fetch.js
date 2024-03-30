92395a192228a700e5c6c6d72435c063728452a6
Anass <ok@oks-MacBook-Pro.local>
2024-03-30 14:06:22 +0000

    Change headers variable to a function that return headers object

--- project/react/src/config/fetch.js
+++ project/react/src/config/fetch.js
@@ -1,7 +1,5 @@
-const jwt_token = localStorage.getItem("jwt_token");
-
 export const endpoint = "http://127.0.0.1:8000/api";
-export const headers = {
+export const headers = () => ({
     "content-type": "application/json",
-    Authorization: `Bearer ${jwt_token}`,
-};
+    Authorization: `Bearer ${localStorage.getItem("jwt_token")}`,
+});
