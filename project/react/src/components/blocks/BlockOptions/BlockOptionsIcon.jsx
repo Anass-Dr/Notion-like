560716a26917746641a9bd5090a6ace5f565ebee
Anass-Dr <anasdrissi77@gmail.com>
2024-04-13 21:20:59 +0000

    Block Options icon component created

---
+++ project/react/src/components/blocks/BlockOptions/BlockOptionsIcon.jsx
@@ -0,0 +1,25 @@
+import { useRef } from "react";
+import "./BlockOptions.css";
+
+function BlockOptionsIcon({ handleOptions, blockId }) {
+    const blockRef = useRef(null);
+
+    const handleBlockOptions = () => {
+        const cords = blockRef.current.getBoundingClientRect();
+        handleOptions(true, cords, blockId);
+    };
+
+    return (
+        <div ref={blockRef} className="block__options-icons">
+            <span className="add">
+                <i className="fa-solid fa-plus"></i>
+            </span>
+            <span onClick={handleBlockOptions} className="show">
+                <i className="fa-solid fa-ellipsis-vertical"></i>
+                <i className="fa-solid fa-ellipsis-vertical"></i>
+            </span>
+        </div>
+    );
+}
+
+export default BlockOptionsIcon;
