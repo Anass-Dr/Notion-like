4ad0013c5d029f90775b1570fb58610d1550fd67
Anass-Dr <anasdrissi77@gmail.com>
2024-04-13 22:42:42 +0000

    File controller created

---
+++ project/app/Http/Controllers/FileController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+
+class FileController extends Controller
+{
+    public function save(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'file' => 'required|file|max:5120',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $file = $request->file('file');
+        $path = Storage::disk('public')->put('uploads', $file);
+        return response()->json([
+            'message' => 'File uploaded successfully!',
+            'path' => $path,
+        ]);
+    }
+}
