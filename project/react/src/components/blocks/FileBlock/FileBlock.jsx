8dfdc198b5f9e7d2c192ca07b67407471a303857
Anass-Dr <anasdrissi77@gmail.com>
2024-04-13 21:19:58 +0000

    File block component created

---
+++ project/react/src/components/blocks/FileBlock/FileBlock.jsx
@@ -0,0 +1,79 @@
+import { useState, useEffect, useRef, useContext } from "react";
+import { BlockOptionsContext } from "../../../context/BlockOptionsContext";
+import FileToolbar from "../FileToolbar/FileToolbar";
+import ImageBlock from "../ImageBlock/ImageBlock";
+import VideoBlock from "../VideoBlock/VideoBlock";
+import AudioBlock from "../AudioBlock/AudioBlock";
+import BlockOptionsIcon from "../BlockOptions/BlockOptionsIcon";
+import "./FileBlock.css";
+
+function FileBlock({ block, title, handleBlock, handleActiveBlock, children }) {
+    const [showToolbar, setToolbar] = useState(false);
+    const [onHover, setOnHover] = useState(false);
+    const blockRef = useRef(null);
+    const { handleOptions } = useContext(BlockOptionsContext);
+
+    const handleFile = (type, src) => {
+        if (type === "link") handleBlock(src);
+    };
+
+    const handleBlockOptionsIcon = (e) => {
+        if (e.type === "mouseenter") {
+            setOnHover(true);
+        } else {
+            setOnHover(false);
+        }
+    };
+
+    let element = "";
+
+    switch (block.type) {
+        case "image":
+            element = <ImageBlock src={block.content} />;
+            break;
+        case "video":
+            element = <VideoBlock src={block.content} />;
+            break;
+        case "audio":
+            element = <AudioBlock src={block.content} />;
+            break;
+    }
+
+    return (
+        <>
+            <div
+                ref={blockRef}
+                className="block"
+                onClick={() => handleActiveBlock(block.id)}
+                data-type={block.type}
+                data-id={block.id}
+                onMouseEnter={handleBlockOptionsIcon}
+                onMouseLeave={handleBlockOptionsIcon}
+            >
+                {onHover && (
+                    <BlockOptionsIcon
+                        blockId={block.id}
+                        handleOptions={handleOptions}
+                    />
+                )}
+                {block.content ? (
+                    element
+                ) : (
+                    <div onClick={() => setToolbar(true)} className="_file">
+                        <div className="icon">{children}</div>
+                        <span>{title}</span>
+                    </div>
+                )}
+            </div>
+            {showToolbar && (
+                <FileToolbar
+                    cords={blockRef.current.getBoundingClientRect()}
+                    setToolbar={setToolbar}
+                    handleFile={handleFile}
+                />
+            )}
+        </>
+    );
+}
+
+export default FileBlock;
