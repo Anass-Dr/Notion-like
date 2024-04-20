9188caf2d1128844195342c906a85d45f9e4febf
Anass-Dr <anasdrissi77@gmail.com>
2024-04-20 17:52:07 +0100

    Modification Added : (handle block delete, handle prompt display, add hover handler and cords to BlockOptionsIcon component)

--- project/react/src/components/blocks/Text.jsx
+++ project/react/src/components/blocks/Text.jsx
@@ -2,7 +2,14 @@ import { useState, useEffect, useRef, useContext } from "react";
 import BlockOptionsIcon from "./BlockOptions/BlockOptionsIcon";
 import { BlockOptionsContext } from "../../context/BlockOptionsContext";
 
-function Text({ block, handleBlock, handleActiveBlock, children }) {
+function Text({
+    block,
+    handleBlock,
+    handleActiveBlock,
+    handleBlockDelete,
+    showPrompt,
+    children,
+}) {
     const [onHover, setOnHover] = useState(false);
     const { handleOptions } = useContext(BlockOptionsContext);
     const divRef = useRef(null);
@@ -19,26 +26,42 @@ function Text({ block, handleBlock, handleActiveBlock, children }) {
         }
     };
 
+    const handleKey = (e) => {
+        if (e.key === "Backspace" && e.currentTarget.textContent.length == 0) {
+            handleBlockDelete(block.id);
+        } else if (e.key === "Enter") {
+            e.preventDefault();
+            showPrompt();
+        }
+    };
+
     return (
-        <div
-            ref={divRef}
-            className={`_${block.type} block`}
-            data-type={`${block.type}`}
-            data-id={block.id}
-            onClick={() => handleActiveBlock(block.id)}
-            onBlur={(e) => handleBlock(e.target.textContent)}
-            onMouseEnter={handleBlockOptionsIcon}
-            onMouseLeave={handleBlockOptionsIcon}
-            contentEditable
-        >
+        <>
+            <div
+                ref={divRef}
+                className={`_${block.type} block`}
+                data-type={`${block.type}`}
+                data-id={block.id}
+                onKeyDown={handleKey}
+                onClick={() => handleActiveBlock(block.id)}
+                onBlur={(e) => handleBlock(e.target.textContent)}
+                onMouseEnter={handleBlockOptionsIcon}
+                onMouseLeave={handleBlockOptionsIcon}
+                contentEditable
+                suppressContentEditableWarning={true}
+            >
+                {children}
+            </div>
             {onHover && (
                 <BlockOptionsIcon
                     blockId={block.id}
                     handleOptions={handleOptions}
+
+                    handleHover={handleBlockOptionsIcon}
+                    cords={divRef.current.getBoundingClientRect()}
                 />
             )}
-            {children}
-        </div>
+        </>
     );
 }
 
