e90cabe8a5e956917849a6c647427735812ceb0b
Anass-Dr <anasdrissi77@gmail.com>
2024-04-21 00:59:19 +0100

    Check user is logged in

--- project/react/src/pages/Workspace/Workspace.jsx
+++ project/react/src/pages/Workspace/Workspace.jsx
@@ -1,13 +1,23 @@
-import { useParams } from "react-router-dom";
+import { useContext } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import Sidebar from "./Sidebar/Sidebar";
 import Header from "./Header/Header";
 import Page from "./Page/Page";
 import { WorkspaceContextProvider } from "../../context/WorkspaceContext";
 import Wrapper from "../../Wrapper";
 import "./main.css";
+import { AuthContext } from "../../context/AuthContext";
+import Loading from "../../components/Loading";
 
 function Workspace() {
+    // const nav = useNavigate();
+    const { loading, isLoggedIn } = useContext(AuthContext);
+    const nav = useNavigate();
     const { username } = useParams();
+
+    if (loading) return <Loading />
+    if (!isLoggedIn) nav("/login");
+
     return (
         <div id="user">
             <div className="container">
