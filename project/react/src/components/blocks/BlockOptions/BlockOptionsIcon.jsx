90a4c9b26df409f61285679d5485c9ab191d4cbd
Anass-Dr <anasdrissi77@gmail.com>
2024-04-20 17:53:26 +0100

    Modification Added : (styles added, handle hover)

--- project/react/src/components/blocks/BlockOptions/BlockOptionsIcon.jsx
+++ project/react/src/components/blocks/BlockOptions/BlockOptionsIcon.jsx
@@ -1,8 +1,15 @@
 import { useRef } from "react";
 import "./BlockOptions.css";
 
-function BlockOptionsIcon({ handleOptions, blockId }) {
+function BlockOptionsIcon({ handleOptions, blockId, cords, handleHover }) {
     const blockRef = useRef(null);
+    
+    const styles = cords && {
+        top: cords.top + (cords.bottom - cords.top) / 2,
+        left: cords.left - 60,
+        padding: "0 1rem",
+        transform: "translateY(-50%)"
+    };
 
     const handleBlockOptions = () => {
         const cords = blockRef.current.getBoundingClientRect();
@@ -10,7 +17,15 @@ function BlockOptionsIcon({ handleOptions, blockId }) {
     };
 
     return (
-        <div ref={blockRef} className="block__options-icons" contentEditable="false">
+        <div
+            style={cords ? styles : {}}
+            ref={blockRef}
+            className="block__options-icons"
+            onMouseEnter={handleHover}
+            onMouseLeave={handleHover}
+            contentEditable="false"
+            suppressContentEditableWarning={true}
+        >
             <span className="add">
                 <i className="fa-solid fa-plus"></i>
             </span>
