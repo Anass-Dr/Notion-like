f64127fe9e510eed955fad17c44c975b904dbd41
Anass-Dr <anasdrissi77@gmail.com>
2024-04-24 23:37:44 +0100

    useEffect import removed

--- project/react/src/components/ShareToolbar/ShareToolbar.jsx
+++ project/react/src/components/ShareToolbar/ShareToolbar.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./ShareToolbar.css";
 
 function ShareToolbar({ cords, setToolbar, handlePublish, handleUnpublish, token }) {
