0d06d245a368a11561f6eb4beb6a931a2e9d1526
Anass-Dr <anasdrissi77@gmail.com>
2024-04-24 23:51:09 +0100

    Modifications added : (WorkspaceContextProvider removed, Consuming WorkspaceContext and AuthContext, use Authorization class, Passing WorkspaceContext data to children )

--- project/react/src/pages/Workspace/Workspace.jsx
+++ project/react/src/pages/Workspace/Workspace.jsx
@@ -3,31 +3,46 @@ import { useParams, useNavigate } from "react-router-dom";
 import Sidebar from "./Sidebar/Sidebar";
 import Header from "./Header/Header";
 import Page from "./Page/Page";
-import { WorkspaceContextProvider } from "../../context/WorkspaceContext";
 import Wrapper from "../../Wrapper";
-import "./main.css";
 import { AuthContext } from "../../context/AuthContext";
 import Loading from "../../components/Loading";
+import { WorkspaceContext } from "../../context/WorkspaceContext";
+import Authorize from "../../helpers/Authorization";
+import "./main.css";
 
 function Workspace() {
     const [showSidebar, setSidebar] = useState(false);
-    const { loading, isLoggedIn } = useContext(AuthContext);
-    const { username } = useParams();
+    const auth = useContext(AuthContext);
+    const workspace = useContext(WorkspaceContext);
+    const currPage = workspace.data.filter((page) => page.active)[0];
+    const params = useParams();
     const nav = useNavigate();
 
-    if (loading) return <Loading />;
-    if (!isLoggedIn) nav("/login");
+    if (auth.loading || workspace.loading) return <Loading />;
+    
+    const authorize = new Authorize(auth, params, workspace.data, nav, workspace.changeActivePage);
+    authorize.handle();
 
     return (
         <div id="user">
             <div className="container">
-                <WorkspaceContextProvider>
-                    <Wrapper>
-                        <Sidebar showSidebar={showSidebar} setSidebar={setSidebar} />
-                        <Header showSidebar={setSidebar} />
-                        <Page />
-                    </Wrapper>
-                </WorkspaceContextProvider>
+                <Wrapper>
+                    <Sidebar
+                        data={workspace.data}
+                        handleNewPage={workspace.handleNewPage}
+                        showSidebar={showSidebar}
+                        setSidebar={setSidebar}
+                    />
+                    <Header
+                        currPage={currPage}
+                        deletePage={workspace.deletePage}
+                        showSidebar={setSidebar}
+                    />
+                    <Page
+                        currPage={currPage}
+                        saveChange={workspace.saveChange}
+                    />
+                </Wrapper>
             </div>
         </div>
     );
